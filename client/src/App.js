f32f143ac49cfd0c0683fab18260a08291ef4538
Piotr Frukacz <pfrukacz@gmail.com>
2019-02-01 09:36:13 +0100

    Add sass and jumbotron component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import './styles/App.css';
+import './styles/App.scss';
+
+import Homepage from './components/Homepage';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Homepage />
+            </div>
+        );
+    }
 }
 
 export default App;
