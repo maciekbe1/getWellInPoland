a4931b0c89e863d3f29c790089201b987812833c
Piotr Frukacz <pfrukacz@gmail.com>
2019-02-06 14:11:39 +0100

    Change navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,27 +4,27 @@ import logo from '../images/logo/logo_male.png';
 const Navbar = () => {
     return (
         <div className="nav-bar container-fluid">
-            <nav class="container-fluid navbar navbar-expand-lg navbar-light bg-light fixed-top">
+            <nav className="container-fluid navbar navbar-expand-lg navbar-light bg-light fixed-top w-100">
                 <div className="container">
-                    <a class="navbar-brand" href="#test"><img src={logo} alt="logo"/></a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                    <a className="navbar-brand" href="#test"><img src={logo} alt="logo"/></a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                     </button>
 
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav ml-auto">
-                            <li class="nav-item active">
-                            <a class="nav-link" href="#test">Home <span class="sr-only">(current)</span></a>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item active">
+                            <a className="nav-link" href="#test">Home <span className="sr-only">(current)</span></a>
                             </li>
-                            <li class="nav-item">
-                            <a class="nav-link" href="#test">Link</a>
+                            <li className="nav-item">
+                            <a className="nav-link" href="#test">Link</a>
                             </li>
-                            <li class="nav-item">
-                            <a class="nav-link disabled" href="#test" tabindex="-1" aria-disabled="true">Disabled</a>
+                            <li className="nav-item">
+                            <a className="nav-link disabled" href="#test" tabIndex="-1" aria-disabled="true">Disabled</a>
                             </li>
                         </ul>
                     </div>
-                </div>            
+                </div>
             </nav>
         </div>
     );
